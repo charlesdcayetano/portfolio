de874dd49774248de81e9d1bca14879df641abd9
charlesdcayetano <cayetanocharlesd92000@gmail.com>
2026-09-08 18:13:53 +0800

    Updates

--- assets/react-vendor-BDqJNCtc.js
+++ assets/react-vendor-BDqJNCtc.js
@@ -1,4 +1,4 @@
-import{r as yc}from"./icons-BmTYRbQl.js";var hi={exports:{}},ve={},yi={exports:{}},gi={};/**
+import{r as yc}from"./icons-CxgTrMS6.js";var hi={exports:{}},ve={},yi={exports:{}},gi={};/**
  * @license React
  * scheduler.production.min.js
  *
