d3a30adcf239260f716410a24ab189be4cce4327
charlesdcayetano <cayetanocharlesd92000@gmail.com>
2026-09-26 17:38:49 +0800

    updating file extention

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -59,7 +59,7 @@ export default function ProfileHeader() {
         </div>
 
         <img
-          src="/chep/images/Portfolio.webp"
+          src="public/images/Portfolio.webp"
           alt="Portrait of Charles D. Cayetano"
           width={140}
           height={140}
